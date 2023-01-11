95fdcc22dd54fd2072625e48b0cb762d5626d2b0
TiDev00 <cisse.thierno@outlook.com>
2023-01-10 20:16:38 -0500

    lexicon update

--- lexicon_building/processing.py
+++ lexicon_building/processing.py
@@ -11,27 +11,31 @@ Contents:
 import utils.wolof_rules as wr
 from utils.spellchecker_utils import word_extraction
 
+exceptions = {'v', 'z', 'h'}
 
-print("Processing wolof text")
+print("Processing wolof texts")
 corpus_words = set(word_extraction('wolof_texts.txt'))
 for i in corpus_words.copy():
-    if not i.isalpha() or wr.fr_en_checking(i) or not wr.rules_validator(i):
+    if not i.isalpha() or i.isupper() or wr.fr_en_checking(i) or not wr.rules_validator(i):
         corpus_words.remove(i)
 
 
 print("Processing wolof dictionary")
 dico_words = set(word_extraction('wolof_dictionary.txt'))
 for j in dico_words.copy():
-    if not j.isalpha() or wr.fr_en_checking(j) or not wr.rules_validator(j):
+    if not j.isalpha() or j.isupper() or wr.fr_en_checking(j) or not wr.rules_validator(j):
         dico_words.remove(j)
 
 glossary = (corpus_words.union(dico_words))
+
+for exception in exceptions:
+    for word in glossary.copy():
+        if exception in word:
+            glossary.remove(word)
 
 
 print("Creating lexicon file")
 with open('../wolof_lexicon.txt', 'w') as f:
     for word in glossary:
-        if 'aaa' not in word and 'h' not in word and 'ï' not in word \
-                and 'v' not in word and 'z' not in word and 'ç' not in word:
-            f.write(word + '\n')
+        f.write(word + '\n')
 
